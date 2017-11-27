36569dfdc854afb05c6d7dee5f834f70a959e913
Dan Allen <dan.j.allen@gmail.com>
2017-11-26 23:53:32 -0700

    fix code style in features env config

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,5 @@
-PROJECT_ROOT_PATH = File.dirname(File.dirname(File.dirname(__FILE__)))
+PROJECT_ROOT_PATH = File.expand_path '../../..', __FILE__
 ENV['TEST'] = 'true'
 require 'middleman-core'
 require 'middleman-core/step_definitions'
-require File.join(PROJECT_ROOT_PATH, 'lib', 'middleman-asciidoc')
+require File.join PROJECT_ROOT_PATH, 'lib', 'middleman-asciidoc'
